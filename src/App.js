4942e19ad5f1a63bfe27bb17f05479075d914bbb
Przemysław Lange <przemek.lange@gmail.com>
2022-06-03 22:18:53 +0200

    new-change
    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import Coins from './Coins';
 
 function App() {
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
+      setCoins(res.data);
+    }).catch(error => console.log(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filterCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLocaleLowerCase()))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coins-app">
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input type='text' placeholder='Search' className='coin-input' onChange={handleChange} />
+        </form>
+      </div>
+      {filterCoins.map(coin => {
+        return (
+          <Coins key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} volume={coin.market_cap}
+            price={coin.current_price} priceChange={coin.price_change_percentage_24h} />
+        )
+      })}
     </div>
   );
 }
---
+++ src/Coins.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Coins({ name, image, symbol, price, volume, priceChange }) {
+    return (
+        <div className='coin-container'>
+            <div className='coin-row'>
+                <div className='coin'>
+                    <img src={image} alt='crypto' />
+                    <h1>{name}</h1>
+                    <p className='coin-symbol'>{symbol}</p>
+                </div>
+                <div className='coin-data'>
+                    <p className='coin-price'>${price}</p>
+                    <p className='coin-volume'>${volume.toLocaleString()}</p>
+                    {priceChange < 0 ? (<p className='coin-precent red'>{priceChange.toFixed(2)}%</p>) :
+                        (<p className='coin-precent green'>{priceChange.toFixed(2)}%</p>)
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Coins
